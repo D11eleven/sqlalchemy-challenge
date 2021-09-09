a55e538f2906d337bd6dfb5e638c59092f0120e9
Darrell S Horich <d11eleven@gmail.com>
2021-09-09 04:36:07 -0500

    incremental save

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 # sqlalchemy-challenge
 
 
- Incremental Save --  2.Climate App Working - api precipitation complete
+ Incremental Save --  2.Climate App Working - start and start/end routes incomplete  
 =======
 ![surfs-up](https://user-images.githubusercontent.com/82190357/131483610-9e04da8f-2caf-4099-aefa-3f7b1b9bd6f1.png)
 # SQLAlchemy Homework - Surfs Up!
--- app.py
+++ app.py
@@ -18,7 +18,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 from sqlalchemy import func
 
-##
+###########################################################################
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 Base = automap_base()
@@ -35,7 +35,7 @@ Measurement = Base.classes.measurement
 
 
 app = Flask(__name__)
-
+###################################################################
 @app.route("/")
 def welcome():
     """List all available api routes."""
@@ -47,13 +47,10 @@ def welcome():
         f"/api/v1.0/<start><br/>"
         f"/api/v1.0/<start>/<end><br/>"
     )
-
+#####################################################################
 @app.route("/api/v1.0/precipitation")
 def precipitation():
-    
-    # recent_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
-    # return recent_date
-
+   
     year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
 
     results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= year_ago).\
@@ -68,35 +65,84 @@ def precipitation():
 
     return_list = jsonify(all_rain)
     return return_list
+    
+######################################################################
+@app.route("/api/v1.0/stations")
+def stations():
+    
+    # recent_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+    # return recent_date
+    results = session.query(Station.id, Station.station, Station.name).all()
+
+    all_stations = []
+    for mid, mstation, mname in results:
+            station_dict = {}
+            station_dict["ID"] = mid
+            station_dict["Station"] = mstation
+            station_dict["Name"] = mname
+            all_stations.append(station_dict)
+
+    return_list = jsonify(all_stations)
+    return return_list
+ 
+##################################################################
+
+@app.route("/api/v1.0/tobs")
+def tobs():
+    
+    
+    year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+
+
+    results = session.query(Measurement.date, Measurement.tobs).\
+        filter(Measurement.date >= year_ago).\
+        filter(Measurement.station == "USC00519281").all()
+
+  
+    all_temps = []
+    for mdate, mtemp in results:
+            temp_dict = {}
+            temp_dict["Date"] = mdate
+            temp_dict["Temp"] = mtemp
+            all_temps.append(temp_dict)
+    
+    
+    return_list = jsonify(all_temps)
+    return return_list
+    session.close()
+# ####################################################
+# @app.route("/api/v1.0/startdate_only")   
+# def summaryS():
+
+#     start_date = (YYYY, M, DD)
+#     end_date = (2017, 8, 23)
+
+
+#  session.query(Measurement.station, 
+#        func.min(Measurement.tobs), 
+#        func.max(Measurement.tobs),
+#        func.avg(Measurement.tobs)).\
+#        filter(Measurement.date >= start_date).\
+#        filter(Measurement.date <= end_date)
+
+# @app.route("/api/v1.0/start_end_dates")   
+# def summarySE():
+
+#     start_date = (YYYY, M, DD)
+#     end_date = (YYYY, M, DD)
+
+
+#  session.query(Measurement.station, 
+#        func.min(Measurement.tobs), 
+#        func.max(Measurement.tobs),
+#        func.avg(Measurement.tobs)).\
+       
+#        filter(Measurement.date >= start_date).\
+#        filter(Measurement.date <= end_date)
 
 
 
-
-
-#     engine = create_engine("sqlite:///../Resources/chinook.sqlite", echo=False)
-#     Base = automap_base()
-#     Base.prepare(engine, reflect=True)
-#     Base.classes.keys()
-#     Invoices = Base.classes.invoices
-#     Items = Base.classes.invoice_items
-#     session = Session(engine)
-#     results = session.query(Invoices.BillingPostalCode, func.sum(Items.UnitPrice * Items.Quantity)).\
-#         filter(Invoices.InvoiceId == Items.InvoiceId).\
-#         filter(Invoices.BillingCountry == 'USA').\
-#         group_by(Invoices.BillingPostalCode).\
-#         order_by(func.sum(Items.UnitPrice * Items.Quantity).desc()).all()
-#     session.close()
-#     # Create a dictionary from the row data and append to a list of all_passengers
-#     all_bills = []
-#     for zip, amount in results:
-#         bill_dict = {}
-#         bill_dict["postal_code"] = zip
-#         bill_dict["amount"] = float(amount)
-#         all_bills.append(bill_dict)
-#     return_list = jsonify(all_bills)    
-#     return return_list
-# if __name__ == "__main__":
-#     app.run())
+session.close()
 
 if __name__ == "__main__":
         # print(home())
