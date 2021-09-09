1b22d8802c8927b3f77d25bf6056e9ab2743629d
Darrell S Horich <d11eleven@gmail.com>
2021-09-09 02:10:55 -0500

    incremental save

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 # sqlalchemy-challenge
 
 
- Incremental Save --  1.Precipitation Analysis and Working Station Analysis Complete   2.Climate App Working 
+ Incremental Save --  2.Climate App Working - api precipitation complete
 =======
 ![surfs-up](https://user-images.githubusercontent.com/82190357/131483610-9e04da8f-2caf-4099-aefa-3f7b1b9bd6f1.png)
 # SQLAlchemy Homework - Surfs Up!
--- app.py
+++ app.py
@@ -18,14 +18,12 @@ from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 from sqlalchemy import func
 
-
-
-
+##
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
 
 Base = automap_base()
 
-# reflect the tables
+    # reflect the tables
 Base.prepare(engine, reflect =True)
 
 Base.classes.keys()
@@ -36,29 +34,70 @@ Station = Base.classes.station
 Measurement = Base.classes.measurement
 
 
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end><br/>"
+    )
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+    
+    # recent_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+    # return recent_date
+
+    year_ago = dt.date(2017, 8, 23) - dt.timedelta(days=365)
+
+    results = session.query(Measurement.date, Measurement.prcp).filter(Measurement.date >= year_ago).\
+        order_by(Measurement.date).all()
+
+    all_rain = []
+    for mdate, mprcp in results:
+            rain_dict = {}
+            rain_dict["date"] = mdate
+            rain_dict["prcp"] = mprcp
+            all_rain.append(rain_dict)
+
+    return_list = jsonify(all_rain)
+    return return_list
 
 
-# # 2. Create an app, being sure to pass __name__
-# app = Flask(__name__)
 
 
-# # 3. Define what to do when a user hits the index route
-# @app.route("/")
-# def home():
-#     print("Server received request for 'Home' page...")
-#     return "Welcome to my 'Home' page!"
 
-
-# # 4. Define what to do when a user hits the /about route
-# @app.route("/about")
-# def about():
-#     print("Server received request for 'About' page...")
-#     return "Welcome to my 'About' page!"
-
-# @app.route("/jsonified")
-# def jsonified():
-#     return jsonify(hello_dict)
+#     engine = create_engine("sqlite:///../Resources/chinook.sqlite", echo=False)
+#     Base = automap_base()
+#     Base.prepare(engine, reflect=True)
+#     Base.classes.keys()
+#     Invoices = Base.classes.invoices
+#     Items = Base.classes.invoice_items
+#     session = Session(engine)
+#     results = session.query(Invoices.BillingPostalCode, func.sum(Items.UnitPrice * Items.Quantity)).\
+#         filter(Invoices.InvoiceId == Items.InvoiceId).\
+#         filter(Invoices.BillingCountry == 'USA').\
+#         group_by(Invoices.BillingPostalCode).\
+#         order_by(func.sum(Items.UnitPrice * Items.Quantity).desc()).all()
+#     session.close()
+#     # Create a dictionary from the row data and append to a list of all_passengers
+#     all_bills = []
+#     for zip, amount in results:
+#         bill_dict = {}
+#         bill_dict["postal_code"] = zip
+#         bill_dict["amount"] = float(amount)
+#         all_bills.append(bill_dict)
+#     return_list = jsonify(all_bills)    
+#     return return_list
+# if __name__ == "__main__":
+#     app.run())
 
 if __name__ == "__main__":
-        print(home())
-#     app.run(debug=True)
+        # print(home())
+    app.run(debug=True)
